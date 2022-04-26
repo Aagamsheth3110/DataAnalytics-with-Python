94b9990cdd0b877122c2b85e9bdadb1081738892
Aagam Sheth <86261564+Aagamsheth3110@users.noreply.github.com>
2022-04-26 22:12:58 +0530

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,20 @@
+# Program to perform illustarte different set operations.
+
+#union, intersection, difference and symmetric difference.
+
+#define two(2) set variable E & N
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+ 
+# set union
+print("Union of E and N is",E | N)
+ 
+# set intersection
+print("Intersection of E and N is",E & N)
+ 
+# set difference
+print("Difference of E and N is",E - N)
+ 
+# set symmetric difference
+print("Symmetric difference of E and N is",E ^ N)
+
